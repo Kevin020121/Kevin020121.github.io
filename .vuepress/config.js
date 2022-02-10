5ce503f5b53e944ec01b8bba066adca854c26ed9
Kevin020121 <1368052450@qq.com>
2022-02-10 15:46:53 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,6 @@
-﻿const moment = require('moment');
+﻿const nav = require('./nav');
+
+const moment = require('moment');
 moment.locale('zh-cn');
 
 module.exports = {
@@ -23,39 +25,7 @@ module.exports = {
   ],
   "theme": "reco",
   "themeConfig": {
-    "nav": [
-      {
-        "text": "主页",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "时间线",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "笔记",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
-        ]
-      },
-      {
-        "text": "联系我",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://https://github.com/Kevin020121/Kevin020121.github.io",
-            "icon": "reco-github"
-          }
-        ]
-      }
-    ],
+    nav,
     "sidebar": {
       
     },
---
+++ .vuepress/nav.js
@@ -0,0 +1,33 @@
+module.exports =  [
+  {
+    "text": "主页",
+    "link": "/",
+    "icon": "reco-home"
+  },
+  {
+    "text": "时间线",
+    "link": "/timeline/",
+    "icon": "reco-date"
+  },
+  {
+    "text": "笔记",
+    "icon": "reco-message",
+    "items": [
+      {
+        "text": "vuepress-reco",
+        "link": "/docs/theme-reco/"
+      }
+    ]
+  },
+  {
+    "text": "联系我",
+    "icon": "reco-message",
+    "items": [
+      {
+        "text": "GitHub",
+        "link": "https://https://github.com/Kevin020121/Kevin020121.github.io",
+        "icon": "reco-github"
+      }
+    ]
+  }
+]
